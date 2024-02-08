40d8d740c7b5698ce78163030c83e734351422b5
Jessica Choi <jesymchoi@gmail.com>
2024-02-08 15:18:10 -0800

    completed last two exercises

--- some-every.js
+++ some-every.js
@@ -69,7 +69,11 @@ Examples:
     hasCertainKey(arr,'isCatOwner') // false
 */
 
-function hasCertainKey(arr, key) {}
+function hasCertainKey(arr, key) {
+  return arr.every(function(arrObj) {
+    return key in arrObj;
+  })
+}
 
 /*
 Write a function called hasCertainValue which accepts an array of objects and a key, and a value, and returns true if every single object in the array contains that value for the specific key. Otherwise it should return false.
@@ -87,4 +91,8 @@ Examples:
     
 */
 
-function hasCertainValue(arr, key, searchValue) {}
+function hasCertainValue(arr, key, searchValue) {
+  return arr.every(function(objArr) {
+    return objArr[key] === searchValue;
+  })
+}
